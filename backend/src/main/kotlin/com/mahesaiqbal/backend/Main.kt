11418dda7013f8fa93927a479b5b2fa29bfbb72b
Mahesa Iqbal Ridwansyah <iqbalridwansyah.ir@gmail.com>
2018-11-08 10:55:50 +0700

    First commit

---
+++ backend/src/main/kotlin/com/mahesaiqbal/backend/Main.kt
@@ -0,0 +1,23 @@
+package com.mahesaiqbal.backend
+
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("Belajar membuat rest api dengan Kotlin")
+            }
+        }
+    }
+    server.start(wait = true)
+}
